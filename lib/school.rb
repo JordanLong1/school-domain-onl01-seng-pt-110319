1e639f3a6941515a185fd5e8a3479beb03021395
Jordan <jordanleelong5@gmail.com>
2019-11-19 21:08:04 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,62 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        # roster["grade"] = [] 
+        # roster["grade"] << name[]
+        if roster[grade]
+        roster[grade] << name
+        else 
+            roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    # def sort
+    #     sorted = {}
+
+    #     roster.each do |grade, name|
+    #         sorted[grade] = name.sort
+
+    #     end
+    #     sorted
+    # end
+
+
+# You should be able to get a sorted list of all the students where the strings in the student arrays are sorted alphabetically. For instance:
+
+
+def sort 
+    sorted = {}
+
+    roster.each do |grade, name|
+        sorted[grade] = name.sort
+    end
+    sorted
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
